fa3dde7d335cf4a6065b32b96077d5cb66518c82
farid07a <faridmaster02@gmail.com>
2026-08-24 00:08:13 +0200

    Add cards

--- js/main.js
+++ js/main.js
@@ -5,56 +5,99 @@ const clubs=[
         id:1,
         name:"Biskra CITY",
         city:"Biskra",
-        sport:"Football"
+        sport:"Football",
+        image: "images/cl1.jpg"
+
     },
     {
         id:2,
-        name:"Biskra USB",
-        city:"Biskra",
-        sport:"Football"
+        name:"Batna USB",
+        city:"Batna",
+        sport:"Tennis",
+        image: "images/cl2.jpg"
     },
     {
         id:3,
         name:"JSD",
         city:"Djemorah",
-        sport:"Football"
+        sport:"KickBoxing",
+        image: "images/cl3.jpg"
     }
 ]
 
-const clubGrid = document.querySelector(".clubs-grid")
-
-console.log(clubGrid);
 
 
-clubs.forEach(item => {
-    const card = document.createElement("article");
-    card.classList.add("club-card");
-    card.innerHTML = `
-    <div class="club-card-content">
-
-        <h2>${item.name}</h2>
-
-        <p>City: ${item.city}</p>
-
-        <p>Sport: ${item.sport}</p>
-
-        <a href="club-details.html?id=${item.id}">View Club</a>
-
-    </div> `;
-
-    clubGrid.appendChild(card);
-});
+    //console.log("clubGrid is null"); // Runs if it was not found
+    //console.log("passe1");
 
 
-console.log("passe1");
 
-const params = new URLSearchParams(window.location.search);
-console.log("passe2");
-const clubId = params.get("id");
-console.log("passe3");
-console.log(clubId);
 
-console.log("passe4");
+
+function renderClubs() {
+    const clubGrid = document.querySelector(".clubs-grid")
+
+
+if (clubGrid) {
+  console.log(clubGrid); // Runs if the element exists
+   console.log(clubGrid);
+
+
+    clubs.forEach(item => {
+        const card = document.createElement("article");
+        card.classList.add("club-card");
+        card.innerHTML = `
+        <img
+            src="${item.image}"
+            alt="${item.name}"
+            class="club-image"
+        >
+
+            <div class="club-card-content">
+
+                <h2>${item.name}</h2>
+
+                <p>City: ${item.city}</p>
+
+                <p>Sport: ${item.sport}</p>
+
+                <a href="club-details.html?id=${item.id}">
+                    View Club
+                </a>
+
+            </div>
+        `;
+
+        clubGrid.appendChild(card);
+    });
+
+}
+
+}
+
+function renderClubDetails(){
+    
+    const params = new URLSearchParams(window.location.search);
+
+    const clubId = Number(params.get("id"));
+
+    const selectedClub = clubs.find(item => item.id === clubId);
+
+    console.log(selectedClub);
+
+
+    const clubName = document.getElementById("club-name");
+    const clubCity = document.getElementById("club-city");
+    const clubSport = document.getElementById("club-sport");
+    const clubImage = document.getElementById("club-image");
+
+    clubName.textContent = selectedClub.name;
+    clubCity.textContent = selectedClub.city;
+    clubSport.textContent = selectedClub.sport;
+    clubImage.src = selectedClub.image;
+    clubImage.alt = selectedClub.name;
+
+}
 
 
 /*
